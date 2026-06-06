769af148cff8de601e39a5f7dbf690850f1d8829
PureBacn <nathanhii007@outlook.com>
2026-06-06 00:54:14 -0700

    basic motor control implementation

--- include/config.h
+++ include/config.h
@@ -1,1 +1,9 @@
 #pragma once
+
+#define LeftEN 0
+#define LeftLPWM 10
+#define LeftRPWM 11
+
+#define RightEN 0
+#define RightLPWM 15
+#define RightRPWM 14
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
 #include <Arduino.h>
+#include "config.h"
+
 #include "BTS7960.h"
+
+BTS7960 LeftController(LeftEN, LeftLPWM, LeftRPWM);
+BTS7960 RightController(RightEN, RightLPWM, RightRPWM);
+
+struct MotorSpeeds {
+	int l;
+	int r;
+};
 
 void setup() {
 	Serial.begin(115200);
 	pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void setSpeed(BTS7960& controller, int speed) {
+	if (speed > 0)
+		controller.TurnRight(speed);
+	else if (speed < 0)
+		controller.TurnLeft(-speed);
+	else
+		controller.Stop();
+}
+
+void move(MotorSpeeds&& speeds) {
+	setSpeed(LeftController, speeds.l);
+	setSpeed(RightController, speeds.r);
 }
 
 void loop() {
@@ -13,4 +37,10 @@ void loop() {
 	digitalWrite(LED_BUILTIN, LOW);
 	Serial.println("Off");
 	delay(1000);
+
+	move(MotorSpeeds{200, 200});
+	delay(5000);
+	move(MotorSpeeds{-200, -200});
+	delay(5000);
+	move(MotorSpeeds{0, 0});
 }
