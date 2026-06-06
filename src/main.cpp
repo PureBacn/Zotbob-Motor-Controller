8a8ca1afdedf3ddb6bbe619f93fe16e3b2558575
PureBacn <nathanhii007@outlook.com>
2026-06-05 21:58:09 -0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include "BTS7960.h"
+
+void setup() {
+	Serial.begin(115200);
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+	digitalWrite(LED_BUILTIN, HIGH);
+	Serial.println("On");
+	delay(1000);
+	digitalWrite(LED_BUILTIN, LOW);
+	Serial.println("Off");
+	delay(1000);
+}
